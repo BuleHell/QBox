6a5d6d4c34c685faba7b13d1f9055a0f7966621c
瓜不甜 <2351180282@qq.com>
2016-03-24 16:32:55 +0800

    基本的协议已经完成

--- QBox/qboxprotocol.cpp
+++ QBox/qboxprotocol.cpp
@@ -7,75 +7,192 @@ QBoxProtocol::QBoxProtocol(QObject *parent) :
     this->block=new QByteArray();
 }
 
-void QBoxProtocol::makeHead(QBOXMESSAGETYPE type, MessageSize size, QString ID, QDateTime time)
+
+void QBoxProtocol::TO_Login(QString username, QString password, QString ID, QBOXMESSAGETYPE type, QDateTime time, MessageSize size)
 {
-    qDebug()<<"----------------PACK----------------";
+    //|--数据大小--|--数据类型--|--发送者ID--|--数据发送时间--|-用户名-|-密码-|
+    //    qDebug()<<"----------------PACK--TOLogin--------------";
     //建造一个输入流
     QDataStream out(this->block, QIODevice::WriteOnly);
     //设定一个版本
     out.setVersion(QDataStream::Qt_4_8);
-
+    //设置协议头
+    //    qDebug()<<"----------------PACK--Header---------------";
     //输入消息内容的大小
-    qDebug()<<"Messagesize:";
-    qDebug()<<(quint16)size;
+    //    qDebug()<<"Messagesize:"<<(quint16)size;
     out<<(quint16)size;
-
     //输入消息类型
-    qDebug()<<"type:";
-    qDebug()<<(quint16)type;
+    //    qDebug()<<"type:"<<(quint16)type;
     out << (quint16)type;
-
     //输入发送的ID
-    qDebug()<<"MessageID:";
-    qDebug()<<ID;
+    //    qDebug()<<"MessageID:"<<ID;
     out<<ID;
-
     //输入发送的时间戳
-    qDebug()<<"MessageTIME:";
-    qDebug()<<time;
+    //    qDebug()<<"MessageTIME:"<<time;
     out<<time;
-
-    //查看输入流和大小
-    qDebug()<<QObject::tr("大小：")<<this->block->size();
+    //    qDebug()<<"----------------OVER--Header---------------";
+    //------------------------------------------------------
+    //    qDebug()<<"----------------body---------------";
+    //用户名
+    out<<username;
+    //密码
+    out<<password;
     //重新填入大小
     out.device()->seek(0);
     out<<(quint16)this->block->size();
+    //查看输入流和大小
+    //    qDebug()<<QObject::tr("大小：")<<this->block->size();
+    //    qDebug()<<"----------------body--over-------------";
+}
+
+void QBoxProtocol::To_beat(QString ID, MYSTATUS status, QBOXMESSAGETYPE type, QDateTime time, MessageSize size)
+{
+    //|--数据大小--|--数据类型--|--发送者ID--|--数据发送时间--|-状态-|
+    //       qDebug()<<"----------------PACK--TOLogin--------------";
+    //建造一个输入流
+    QDataStream out(this->block, QIODevice::WriteOnly);
+    //设定一个版本
+    out.setVersion(QDataStream::Qt_4_8);
+    //设置协议头
+    //        qDebug()<<"----------------PACK--Header---------------";
+    //输入消息内容的大小
+    //        qDebug()<<"Messagesize:"<<(quint16)size;
+    out<<(quint16)size;
+    //        输入消息类型
+    //        qDebug()<<"type:"<<(quint16)type;
+    out << (quint16)type;
+    //输入发送的ID
+    //        qDebug()<<"MessageID:"<<ID;
+    out<<ID;
+    //输入发送的时间戳
+    //        qDebug()<<"MessageTIME:"<<time;
+    out<<time;
+    //        qDebug()<<"----------------OVER--Header---------------";
+    //------------------------------------------------------
+    //        qDebug()<<"----------------body---------------";
+    //状态
+    out<<(quint16)status;
+    //重新填入大小
+    out.device()->seek(0);
+    out<<(quint16)this->block->size();
+    //查看输入流和大小
+    //        qDebug()<<QObject::tr("大小：")<<this->block->size();
+    //        qDebug()<<"----------------body--over-------------";
+}
+
+void QBoxProtocol::To_Message(QString ID, QString receiver, QString Message, QBOXMESSAGETYPE type, QDateTime time, MessageSize size)
+{
+    //|--数据大小--|--数据类型--|--发送者ID--|--数据发送时间--|-接受者ID-|-内容-|
+    //  qDebug()<<"----------------PACK--TOLogin--------------";
+    //建造一个输入流
+    QDataStream out(this->block, QIODevice::WriteOnly);
+    //设定一个版本
+    out.setVersion(QDataStream::Qt_4_8);
+    //设置协议头
+    //    qDebug()<<"----------------PACK--Header---------------";
+    //输入消息内容的大小
+    //    qDebug()<<"Messagesize:"<<(quint16)size;
+    out<<(quint16)size;
+    //输入消息类型
+    //    qDebug()<<"type:"<<(quint16)type;
+    out << (quint16)type;
+    //输入发送的ID
+    //    qDebug()<<"MessageID:"<<ID;
+    out<<ID;
+    //输入发送的时间戳
+    //    qDebug()<<"MessageTIME:"<<time;
+    out<<time;
+    //    qDebug()<<"----------------OVER--Header---------------";
+    //    ------------------------------------------------------
+    //    qDebug()<<"----------------body---------------";
+    //接受者ID
+    out<<receiver;
+    //消息内容
+    out<<Message;
+    //重新填入大小
+    out.device()->seek(0);
+    out<<(quint16)this->block->size();
+    //查看输入流和大小
+    //    qDebug()<<QObject::tr("大小：")<<this->block->size();
+    //    qDebug()<<"----------------body--over-------------";
+
+
 
 }
 
-void QBoxProtocol::openHead(QByteArray *block, quint16 &size, quint16 &type, QString &ID, QDateTime &time)
+void QBoxProtocol::Open(QByteArray *block)
 {
-    qDebug()<<"----------------OPEN----------------";
+    qDebug()<<tr("----------------WWWWW----------------");
     //建造一个输出流
     QDataStream in(block, QIODevice::ReadOnly);
     //设定一个版本
     in.setVersion(QDataStream::Qt_4_8);
+    //开始解析
+    if(block->isEmpty()){
+        qDebug()<<"Empty";
+    }
+    //先把头拿出来
+    quint16 _size;
+    quint16 _type;
+    QBOXMESSAGETYPE TYPE;
+    QString _ID;
+    QDateTime _time;
+    in>>_size;
+    in>>_type;
+    in>>_ID;
+    in>>_time;
+    TYPE=(QBOXMESSAGETYPE)_type;
+    switch (TYPE) {
+    case LOGIN:
+    {
+        qDebug("Login");
+        //|-用户名-|-密码-|
+        QString name;
+        QString pswd;
+        in>>name;
+        in>>pswd;
+        qDebug()<<"name:"<<name<<"password"<<pswd<<"ID:"<<_ID<<"time:"<<_time;
+        //把解析的内容发出去
+        emit TO_Login_SLOT(name,pswd,_ID, _time);
+        break;
+    }
+    case BEAT:
+    {
+        qDebug("BEAT");
+        //|-状态-|
+        MYSTATUS status;
+        quint16 ss;
+        in>>ss;
+        status=(MYSTATUS)ss;
+        qDebug()<<"ID:"<<_ID<<"Status"<<status<<"time:"<<_time;
+        //把解析的内容发出去
+        emit  To_beat_SLOT(_ID,status,_time);
+        break;
+    }
+    case MESSAGE:
+    {
+        //|-接受者ID-|-内容-|
+        QString receiver;
+        QString message;
+        in>>receiver;
+        in>>message;
+        qDebug()<<"ID:"<<_ID<<"receiver"<<receiver<<"message"<<message<<"time:"<<_time;
+        emit  To_Message_SLOT(_ID,receiver,message,_time);
+        break;
+    }
 
-//输入消息内容的大小
-//    quint16 size;
-    in>>size;
-    qDebug()<<"Messagesize:";
-    qDebug()<<size;
+    default:
+    {
+        qDebug()<<QObject::tr("没有该种消息，或者消息结构体被损坏");
+        break;
+    }
+
+    }
 
 
-    //输入消息类型
-  //  quint16 type;
-    in>>type;
-    qDebug()<<"type:";
-    qDebug()<<type;
 
-    //输入发送的ID
-//    QString ID;
-    in>>ID;
-    qDebug()<<"MessageID:";
-    qDebug()<<ID;
-
-    //输入发送的时间戳
-//    qint64 stamp;
-    in>>time;
-    qDebug()<<"MessageTIME:";
-    qDebug()<<time;
 }
+
 
 QByteArray *QBoxProtocol::getBlock()
 {
@@ -87,29 +204,7 @@ void QBoxProtocol::setBlock(QByteArray *b)
     this->block=b;
 }
 
-void QBoxProtocol::makeBody(QBOXMESSAGETYPE type, QByteArray content)
-{
-    switch (type)
-    {
-    case LOGIN://登录消息
-    {
 
-        break;
-    }
-    case MESSAGE://
-    {
-        break;
-
-    }
-    default:
-    {
-        qDebug()<<"ERROR";
-    }
-
-    }
-
-
-}
 
 
 
--- QBox/qboxprotocol.h
+++ QBox/qboxprotocol.h
@@ -4,30 +4,46 @@
 #include <QObject>
 #include<QDataStream>
 #include<QtGui>
-/*
- *MyQBox协议
+//MyQBox协议
+////协议头
+////----------------------------------
+//|--数据大小--|：MessageSize   quint16      16个bit    ：消息头的大小
+//|--数据类型--|：MessageType   quint16      16个bit
+//|--发送者ID--|：senderID      QString      （任意bit）
+//|--数据发送时间--|：MsgTimeStamp quint64   64个bit
+////----------------------------------
+//客户端
+//to:登录：
+//|-内容大小-|--用户名--|--用户密码--|
+//to:心跳：
+//|-用户ID-|
+//to:消息传送
+//|-发送者-|-接受者-|-消息类型-|-消息内容-|
+//to：文件传送
+//|-发送者-|-接受者-|-文件大小|-文件名-|-文件内容-|
+////协议数据
 
-
-//协议头
-//----------------------------------
-|--数据大小--|：MessageSize   quint16      16个bit    ：消息头的大小
-|--数据类型--|：MessageType   quint16      16个bit
-|--发送者ID--|：senderID      QString      （任意bit）
-|--数据发送时间--|：MsgTimeStamp quint64   64个bit
-//----------------------------------
-
-
-//协议数据
-*/
 
 //发送者的消息
 enum QBOXMESSAGETYPE
 {
-    LOGIN=1,//新加入
+    LOGIN=1,   //登录
+    BACK_LOGIN,//登录返回
+    //--------------------
+    BEAT,       //心跳
+    BACK_BEAT,  //心跳返回
+    //------------------------
     MESSAGE,//发送消息
     GETADDRESSANDPORT,//获取通信ip和端口
     FRIENDLIST,//获取好友列表    //其它从这里加入
     LOGOUT//离开
+};
+//我的状态
+enum MYSTATUS
+{
+    ONLINE=1,//在线
+
+    OFFLINE//离线
 };
 
 //发送者的消息的大小（只包含消息的内容，因为其它的这些都是固定的大小）
@@ -39,27 +55,31 @@ class QBoxProtocol : public QObject
 
 public:
     explicit QBoxProtocol(QObject *parent = 0);
-    //-------------------------------------------------
-    //制作协议头（true：消息头正确了 false：大小字段没填）
-    void makeHead(QBOXMESSAGETYPE type, MessageSize size, QString ID, QDateTime time);
-    //制作协议主体
-    void makeBody(QBOXMESSAGETYPE type,QByteArray content);
-    //--------------------------------------------------
-    //打开协议头
-    void openHead(QByteArray *block, quint16 &size,  quint16 &type, QString &ID, QDateTime &time);
-    //打开协议主体
-    void openBody();
+    //------------制作协议----------------------
+    //登录协议
+    void  TO_Login(QString username,QString password,QString ID,QBOXMESSAGETYPE type=LOGIN, QDateTime time=QDateTime::currentDateTime(), MessageSize size=0);
+    //BACK:|-登录返回-|
 
+    //心跳协议
+    void To_beat(QString ID,MYSTATUS status,QBOXMESSAGETYPE type=BEAT, QDateTime time=QDateTime::currentDateTime(), MessageSize size=0);
+    //
+    //发送消息(私聊消息)
+    void To_Message(QString ID,QString receiver,QString Message,QBOXMESSAGETYPE type=MESSAGE, QDateTime time=QDateTime::currentDateTime(), MessageSize size=0);
+    //BACK:|-登录返回-|
 
-
-
+    void Open(QByteArray *block);//主要的解析函数
 public:
     //流的操作
     QByteArray *getBlock();
     void setBlock(QByteArray *b);
 signals:
-   //所有的协议内容解析完之后使用信号的方式发送出去
-
+    //所有的协议内容解析完之后使用信号的方式发送出去
+    //解析完登录
+    void TO_Login_SLOT(QString username,QString password,QString ID, QDateTime time);
+    //解析玩心跳
+    void To_beat_SLOT(QString ID,MYSTATUS status, QDateTime time);
+    //解析完消息
+    void To_Message_SLOT(QString ID,QString receiver,QString Message, QDateTime time);
 public slots:
 
 private:
